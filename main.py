caa9bc79a4dabcf804e4606ad148e79d3a0e5a2d
michciook <michtabiznes@gmail.com>
2023-06-13 00:14:50 +0200

    Added basic movement

--- main.py
+++ main.py
@@ -1,16 +1,63 @@
-# This is a sample Python script.
+import pygame
+from sys import exit
+import math
+from settings import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+pygame.init()
+
+# Tworzenie okna
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Realm Of The *** ***")
+clock = pygame.time.Clock()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("0.png").convert_alpha()
+        self.pos = pygame.math.Vector2(PLAYER_START_X, PLAYER_START_Y)
+        self.speed = PLAYER_SPEED
+
+    def user_input(self):
+        self.velocity_x = 0
+        self.velocity_y = 0
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_w]:
+            self.velocity_y = -self.speed
+        if keys[pygame.K_s]:
+            self.velocity_y = self.speed
+        if keys[pygame.K_a]:
+            self.velocity_x = -self.speed
+        if keys[pygame.K_d]:
+            self.velocity_x = self.speed
+
+        # obsługa poruszania się po przekątnej
+        if self.velocity_x != 0 and self.velocity_y != 0:
+            self.velocity_x /= math.sqrt(2)
+            self.velocity_y /= math.sqrt(2)
+
+    def move(self):
+        self.pos += pygame.math.Vector2(self.velocity_x, self.velocity_y)
+
+    def update(self):
+        self.user_input()
+        self.move()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+player = Player()
+color = (255, 0, 0)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+while True:
+    keys = pygame.key.get_pressed()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.fill(color)
+    pygame.display.update()
+    player.update()
+    screen.blit(player.image, player.pos)
+    clock.tick(FPS)
